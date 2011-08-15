2be2491e19a764a01b172059f85daafb512a78a5
Andrey Petrov <andrey.petrov@shazow.net>
2011-08-14 18:15:19 -0700

    build fix.

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ COMPILER_CMD="java -jar closure/compiler.jar"
 OUTPUT_FLAG="--js_output_file unstdlib.js"
 #EXTRA_FLAGS="--compilation_level ADVANCED_OPTIMIZATIONS"
 
-if [ ! -d closure ]; then
+if [ ! -f "closure/compiler.jar" ]; then
     echo "Closure compiler not found, downloading..."
     mkdir closure
-    wget http://closure-compiler.googlecode.com/files/compiler-latest.tar.gz -O- | tar -xzvf -
+    wget http://closure-compiler.googlecode.com/files/compiler-latest.tar.gz -O- | tar -xzv -f- -C closure
 fi
 
 targets=""
@@ -16,7 +16,7 @@ for t in src/*.js; do
 done
 
 echo -n "Compiling... "
-$COMPILER_CMD $OUTPUT_FLAG $EXTRA_FLAGS $targets
+$COMPILER_CMD $OUTPUT_FLAG $EXTRA_FLAGS $targets || exit -1
 echo "Done."
 
 echo "Warning: monkeypatch files are not included by default."
